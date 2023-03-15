56aa0b70ce5a58ca323102fb9da003bdcc8cb79f
martin-ngigi <martinwainaina001@gmail.com>
2023-03-15 05:31:29 +0300

    Show featured in vertical order

--- lib/widgets/featured_heading.dart
+++ lib/widgets/featured_heading.dart
@@ -17,7 +17,33 @@ class FeaturedHeading extends StatelessWidget {
         left: screenSize.width / 15,
         right: screenSize.width / 15,
       ),
-      child:  Row(
+      //if screen size is less than 800, then its a mobile device
+      //if screen size is < 800 , then show Row for mobile, else show row
+      child: screenSize.width<800? Row(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Featured',
+                style: TextStyle(
+                    fontSize: 36,
+                    fontFamily: 'Raleway',
+                    fontWeight: FontWeight.bold,
+                    color:Color(0xFF077bd7)
+                ),
+              ),
+              SizedBox(height: 5,),
+              Text(
+                'Clue of the wooden cottage',
+                textAlign: TextAlign.end,
+              ),
+            ],
+          ),
+        ],
+      ):
+      Row(
               mainAxisSize: MainAxisSize.max,
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
